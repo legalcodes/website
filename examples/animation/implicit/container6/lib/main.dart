8d9bbbe4d0c1b3d08ac83fe5a33ecfc8a95ea456
Jon Tippens <jtippens@google.com>
2019-11-18 17:56:08 -0800

    Remove extra lines causing unneeded diff

--- examples/animation/implicit/container6/lib/main.dart
+++ examples/animation/implicit/container6/lib/main.dart
@@ -7,7 +7,6 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-
 const _duration = Duration(milliseconds: 400);
 
 double randomBorderRadius() {
